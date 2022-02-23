0fb7a95339408e9350ecc9f69cf6598cb941dec9
calum-atkins <calum2atkins@gmail.com>
2022-02-23 15:07:13 +0000

    Add Map API to display map to the UI

--- app/src/main/java/com/example/atkex/MainActivity.kt
+++ app/src/main/java/com/example/atkex/MainActivity.kt
@@ -4,13 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap : GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+            googleMap = it
+        })
     }
 
     fun onClickLeaderboards(view: View) {
